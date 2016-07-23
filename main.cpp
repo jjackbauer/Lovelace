17bbde0e9ac101b0a8bf112d0d5726598cab0e93
Ruan Bahia <ruanmed@live.com>
2016-07-23 15:14:33 -0300

    Subtração

--- main.cpp
+++ main.cpp
@@ -4,8 +4,22 @@ using namespace std;
 
 int main()
 {
+
+	Lovelace Num1, Num2, Num3;
+
+	Num1 = 487833;
+	Num2 = 9689;
+	Num3 = 0;
+
+	Num3 = Num1 - Num2;
+
+	Num3.imprimir();
+
+	getchar();
+
 	//Contador///*
-	Lovelace A,B,C,D;
+	/*Lovelace A, B, C, D;
+	Lovelace Num1, Num2, Num3;
 	int c;
 
 	A = 1000;
@@ -16,16 +30,24 @@ int main()
 
 	D = 374;
 
-	cout 	<< (A<B) << endl
-			<< (A<C) << endl
-			<< (D<C) << endl
-			<< (B<A) << endl;
+	Num1 = 487233;
+	Num2 = 9689;
+	Num3 = 0;
+
+	Num3 = Num1 - Num2;
+
+	cout << "O valor de Num3 (Num1 - Num2) 3: " << Num3 << endl;
+
+	cout << (A<B) << endl
+		<< (A<C) << endl
+		<< (D<C) << endl
+		<< (B<A) << endl;
 
 
 	getchar();
 
-	A=126679823;
-	B=200000;
+	A = 126679823;
+	B = 200000;
 	A.imprimir();
 	B.imprimir();
 	getchar();
@@ -36,56 +58,56 @@ int main()
 	cout << "Antes do GETCHAR" << endl;
 	getchar();
 
-	/*A.SetDigito(0,0);
-		A.SetDigito(1,1);//*///10 em 10
+	A.SetDigito(0,0);
+	A.SetDigito(1,1);//*///10 em 10
 
-		/*A.setDigito(0,1);//1 em 1
+	/*A.setDigito(0,1);//1 em 1
 
-		B.setDigito(0,3);
+	B.setDigito(0,3);
 
-		C.setDigito(0,3);
-
-
-		cout << "A " << ((A==B)?"":"nao ") << "eh igual a B" << endl;
-		cout << "A " << ((A==C)?"":"nao ") << "eh igual a C" << endl;
+	C.setDigito(0,3);
 
 
-		cout << "A " << ((A>C)?"":"nao ") << "eh maior a C" << endl;
-
-		cout << "A " << ((A>=C)?"":"nao ") << "eh maior ou igual a C" << endl;
-
-		cout << "C " << ((C>=A)?"":"nao ") << "eh maior ou igual a A" << endl;
-
-		cout << "C " << ((C>=B)?"":"nao ") << "eh maior ou igual a B" << endl;
-
-		getchar();//*/
-		//*
-		/*while(1){
-			B =A++;
-			cout<<"A"<<endl;
-			A.imprimir();
-			cout<<"B"<<endl;
-			B.imprimir();
-			getchar();
-
-		}
-		//*///FIM CONTADOR
+	cout << "A " << ((A==B)?"":"nao ") << "eh igual a B" << endl;
+	cout << "A " << ((A==C)?"":"nao ") << "eh igual a C" << endl;
 
 
-		//*/
-		/*
-		A.SetDigito(0,9);
-		A.SetDigito(1,9);
-		A.SetDigito(2,9);
+	cout << "A " << ((A>C)?"":"nao ") << "eh maior a C" << endl;
 
-		B.SetDigito(0,9);
+	cout << "A " << ((A>=C)?"":"nao ") << "eh maior ou igual a C" << endl;
 
-		A = A.Soma(&A,&B);
-		*/
-		/*A.imprimir();
-		B.imprimir();
-		A = A.Soma(&A,&B);
-		A.imprimir();//*/
+	cout << "C " << ((C>=A)?"":"nao ") << "eh maior ou igual a A" << endl;
+
+	cout << "C " << ((C>=B)?"":"nao ") << "eh maior ou igual a B" << endl;
+
+	getchar();//*/
+	//*
+	/*while(1){
+	B =A++;
+	cout<<"A"<<endl;
+	A.imprimir();
+	cout<<"B"<<endl;
+	B.imprimir();
+	getchar();
+
+	}
+	//*///FIM CONTADOR
+
+
+	//*/
+	/*
+	A.SetDigito(0,9);
+	A.SetDigito(1,9);
+	A.SetDigito(2,9);
+
+	B.SetDigito(0,9);
+
+	A = A.Soma(&A,&B);
+	*/
+	/*A.imprimir();
+	B.imprimir();
+	A = A.Soma(&A,&B);
+	A.imprimir();//*/
 
 	return 0;
 }
