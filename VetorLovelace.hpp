d2094ff472d2a002815fd9fb31499f9e61241c93
Ruan Bahia <ruanmed@live.com>
2016-08-18 11:55:57 -0300

    Definições dos cabeçalhos VetorLovelace e VetorMultidimensionalLovelace

--- VetorLovelace.hpp
+++ VetorLovelace.hpp
@@ -1,6 +1,22 @@
 #ifndef VETORLOVELACE_HPP_
 #define VETORLOVELACE_HPP_
-
+class VetorLovelace {
+	private:
+		long long int dimensionalidade;
+		RealLovelace *elementos;
+	public:
+		VetorLovelace();
+		~VetorLovelace();
+		void imprimir();
+		RealLovelace 	getElemento(long long int posicao);
+		void 			setElemento(long long int posicao, RealLovelace X);
+		long long int 	getDimensionalidade();
+		void			setDimensionalidade(long long int novaDimensionalidade);
+		VetorLovelace	somar(const VetorLovelace &B) const;
+		VetorLovelace	subtrair(const VetorLovelace &B) const;
+		RealLovelace 	produtoInterno(const VetorLovelace &B) const;
+		VetorLovelace	multiplicar(const RealLovelace &K) const;
+};
 
 
 
--- VetorMultidimensionalLovelace.hpp
+++ VetorMultidimensionalLovelace.hpp
@@ -1,7 +1,23 @@
 #ifndef VETORMULTIDIMENSIONALLOVELACE_HPP_
 #define VETORMULTIDIMENSIONALLOVELACE_HPP_
 
-
+class VetorMuldimensionalLovelace {
+	private:
+		long long int multidimensao;
+		VetorLovelace dimensoes;
+	public:
+		VetorMuldimensionalLovelace();
+		~VetorMuldimensionalLovelace();
+		void imprimir();
+		long long int 	getMultidimensao();
+		void 			setMultidimensao(long long int novaMultidimensao);
+		RealLovelace 	getElemento(long long int dimensao, long long int posicao);
+		void 			setElemento(long long int dimensao, long long int posicao, RealLovelace X);
+		VetorMuldimensionalLovelace somar(const VetorMuldimensionalLovelace &B) const;
+		VetorMuldimensionalLovelace subtrair(const VetorMuldimensionalLovelace &B) const;
+		VetorMuldimensionalLovelace multiplicar(const RealLovelace &K) const;
+		VetorMuldimensionalLovelace multipliar(const VetorMuldimensionalLovelace &B) const;
+};
 
 
 
